7d8d92f59845c5f4d9f8273e0b71f7b0db6893ec
Zoey <53253298+luvHaze@users.noreply.github.com>
2020-06-11 13:59:55 +0900

    동전 0 (성공)

--- src/BackJoon/Greedy/e11047 (Coin).kt
+++ src/BackJoon/Greedy/e11047 (Coin).kt
@@ -2,6 +2,9 @@ package BackJoon.Greedy
 
 import java.util.*
 
+/* - 백준 11047번 문제 (탐욕기법)
+ * - 동전0
+ */
 fun main() {
     var input = Scanner(System.`in`)
     var N = input.nextInt()
@@ -22,11 +25,8 @@ fun main() {
 }
 
 /*
-*        * 런타임 에러의 원인 -> readLine() 에서 Scanner로 해주니 해결됐다.
-*        * 코틀린은 다른언어에 비해서 실행시간이 길다고 느꼈다.
+*        - 런타임 에러의 원인 -> readLine() 에서 Scanner로 해주니 해결됐다.
+*        - 코틀린은 다른언어에 비해서 실행시간이 길다고 느꼈다.
+*          BufferedReader 를 사용하면 실행시간이 많이 줄어든다고 한다.
 *
-*
-*
-*
-*
- */
+*/
