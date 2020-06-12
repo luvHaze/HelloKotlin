d872750563b6efabb1391f80dc740fd9ef9199c6
Zoey <53253298+luvHaze@users.noreply.github.com>
2020-06-12 22:30:33 +0900

    2020-06-12 E1931 (회의실배정) 해결 못함

---
+++ src/BackJoon/Greedy/e1931 (ConferenceRoom).kt
@@ -0,0 +1,39 @@
+package BackJoon.Greedy
+
+import java.util.*
+
+/*
+*
+*   미해결
+*
+*
+ */
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    val conferenceNum = sc.nextInt()
+    var conferenceTime = arrayListOf<Pair<Int,Int>>()
+    var roomSchedule = hashMapOf<Int, Int>()
+    var answer = 0
+
+    for (i in 1..conferenceNum) {
+        conferenceTime.add(Pair(sc.nextInt(),sc.nextInt()))
+    }
+
+    var sorted = conferenceTime.sortedBy { it.second }
+
+    sorted.forEach {
+        var count = 1
+        var nextStep = it.second
+
+        for(i in sorted.indices){
+            if(nextStep<=sorted[i].first){
+                count++
+                nextStep=sorted[i].second
+            }
+        }
+        if(answer<=count) answer=count
+    }
+
+    print(answer)
+}
