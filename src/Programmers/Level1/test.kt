ae5222aa9377a1483cc4129fb79cddb512e364a1
Zoey <53253298+luvHaze@users.noreply.github.com>
2020-06-19 23:50:41 +0900

    2020-06-19 수준을 깨닫고 Level 1 부터 시작

---
+++ src/Programmers/Level1/test.kt
@@ -0,0 +1,21 @@
+import java.lang.StringBuilder
+import java.util.*
+
+fun main(){
+
+    var arr = intArrayOf(1,2,3,4,5)
+    var alist = arrayListOf<String>()
+    alist.joinToString()
+    var string = "fffdg"
+    var s : Long = 13
+    var a = StringBuilder()
+    var ab = s.toString()
+    alist.sortDescending()
+    ab.forEach {
+
+    }
+    alist.toString().toLong()
+
+    print(3>4 ?"0":"1")
+    string.toArr
+}
