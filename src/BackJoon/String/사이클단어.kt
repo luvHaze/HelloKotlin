071aad299a04c644983340b173a5a3c6e05afbf8
Zoey <53253298+luvHaze@users.noreply.github.com>
2020-07-10 21:36:04 +0900

    2020-07-08
    카카오 키패드, 괄호변환 (Level2)
    각각 해쉬, 재귀함수 사용

---
+++ src/BackJoon/String/사이클단어.kt
@@ -0,0 +1,18 @@
+package BackJoon.String
+
+fun main(){
+    val sin = System.`in`.bufferedReader()
+    val N = sin.readLine().toInt()
+    var arr = arrayListOf<String>()
+    var hash = hashMapOf<String,Int>()
+
+    for(n in 1..N) arr.add(sin.readLine())
+
+    for (i in arr.indices){
+        var temp = arr[i].toCharArray().sorted()
+        arr[i] = temp.joinToString("")
+        hash[arr[i]] = 1
+    }
+
+    print(hash.keys.size)
+}
